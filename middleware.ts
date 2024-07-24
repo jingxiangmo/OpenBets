d3a3b4077fbcbf1b67077acd54be05d1079db189
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-23 18:43:13 -0700

    call supabase middleware from clerk middleware - use both
    https://clerk.com/docs/references/nextjs/clerk-middleware#combine-middleware

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,23 @@
+import { updateSession } from "@/utils/supabase/middleware";
+
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+export default clerkMiddleware(async (auth, req) => {
+  return await updateSession(req);
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+
     "/(api|trpc)(.*)",
   ],
 };
