0d59fed64874b913cc90ed1f633503195d563fa0
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-24 15:49:22 -0700

    strip out unneeded stuff from main page

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,6 @@ import BetForm from "@/components/BetForm";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const isSupabaseConnected = canInitSupabaseClient();
-
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
