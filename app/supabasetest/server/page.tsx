ebee41e337c448db09ddb220e96df6ec188e075e
Jingxiang Mo <jingxiangmo@gmail.com>
2024-07-25 11:09:06 -0700

    server insert example

--- app/supabasetest/server/page.tsx
+++ app/supabasetest/server/page.tsx
@@ -1,5 +1,5 @@
 import { createClerkSupabaseClient } from "@/utils/supabase/server"
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 
 export default async function SupabaseTest() {
   const authstate = auth();
@@ -7,5 +7,21 @@ export default async function SupabaseTest() {
 
   const stuff = await client.from("bet").select("*");
 
+  const userId = authstate.userId;
+
   console.log(stuff.data);
+
+  const {data, error } = await client
+  .from('bet')
+  .insert(
+    {
+      title: "test",
+      affirmative_user_clerk_ids: [userId],
+      affirmative_user_wagers: [100],
+      negative_user_clerk_ids: [userId],
+      negative_user_wagers: [100],
+    }
+  )
+  .select()
+
 }
