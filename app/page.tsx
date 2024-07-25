34bdcb43edfa4532877ebed294cf65ac6aeed9c2
Jingxiang Mo <jingxiangmo@gmail.com>
2024-07-24 18:13:51 -0700

    Refactored template

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClerkSupabaseClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
-import Header from "@/components/Header";
 import BetForm from "@/components/BetForm";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import PastBets from "@/components/PastBets";
