b4c4e5a91c7d83992c60d1745889c288a695e4b0
Jingxiang Mo <jingxiangmo@gmail.com>
2024-07-24 18:22:21 -0700

    Refactor and deleted some files lol

--- app/notes/page.tsx
+++
@@ -1,8 +0,0 @@
-  import { createClerkSupabaseClient } from '@/utils/supabase/server';
-
-  export default async function Notes() {
-    const supabase = createClerkSupabaseClient();
-    const { data: notes } = await supabase.from("deletemeasdf").select();
-
-    return <pre>{JSON.stringify(notes, null, 2)}</pre>
-  }
