a730a21b4456704a7437e8d6985cc4dc5af96c7c
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-26 15:13:55 -0700

    betpage: pull all information from bet into an object

--- app/bet/[betId]/page.tsx
+++ app/bet/[betId]/page.tsx
@@ -3,8 +3,22 @@
 import { createBrowserClient } from "@supabase/ssr";
 import { useState, useEffect } from "react";
 
+interface BetInfo {
+  username: string;
+  wager: number;
+}
+
+interface BetsInfo {
+  title: string;
+  resolveCond?: string;
+  resolveStatus: number; // 0 = unresolved, 1 = resolved affirmatively, 2 = resolved negatively
+  affirmativeBets: BetInfo[];
+  negativeBets: BetInfo[];
+  pot: number;
+}
+
 export default function ViewBet({ params }: { params: { betId: number } }) {
-  const [bet, setBet] = useState<any | null>(null);
+  const [bets, setBets] = useState<BetsInfo | undefined>(undefined);
 
   // TODO: deduplicate
   const supabase = createBrowserClient(
@@ -14,14 +28,61 @@ export default function ViewBet({ params }: { params: { betId: number } }) {
 
   useEffect(() => {
     async function fetchData() {
-      const { error, data } = await supabase.from("bet").select("*").eq("id", params.betId).single();
-      setBet(data);
+      const { error, data } = await supabase
+        .from("bet")
+        .select("*")
+        .eq("id", params.betId)
+        .single();
 
-      console.log("error and data are: ", error, data.title);
+      if (error) {
+        // TODO: handle error
+      }
+
+      function sum(nums: number[]) {
+        return nums.reduce((acc: number, val: number) => acc + val, 0);
+      }
+
+      const pot =
+        sum(data.affirmative_user_wagers) + sum(data.negative_user_wagers);
+
+      if (
+        data.affirmative_user_wagers.length !=
+          data.affirmative_user_clerk_ids.length ||
+        data.negative_user_wagers.length != data.negative_user_clerk_ids.length
+      ) {
+        throw new Error("Mismatched user wager and clerk ID arrays");
+      }
+
+      let affirmativeBets: BetInfo[] = [];
+      for (let i = 0; i < data.affirmative_user_wagers.length; i++) {
+        affirmativeBets.push({
+          username: data.affirmative_user_clerk_ids[i],
+          wager: data.affirmative_user_wagers[i],
+        });
+      }
+
+      let negativeBets: BetInfo[] = [];
+      for (let i = 0; i < data.negative_user_wagers.length; i++) {
+        negativeBets.push({
+          username: data.negative_user_clerk_ids[i],
+          wager: data.negative_user_wagers[i],
+        });
+      }
+
+      const bets: BetsInfo = {
+        title: data.title,
+        resolveCond: data.resolve_cond,
+        resolveStatus: data.resolve_status,
+        affirmativeBets,
+        negativeBets,
+        pot,
+      };
+
+      setBets(bets);
     }
 
     fetchData();
   }, []);
 
-  return <div>{bet?.title}</div>;
+  return <div></div>;
 }
