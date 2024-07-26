659dfe7b1913f6215ef7317c8d52903db04423ca
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-26 13:59:19 -0700

    get bet info on screen

--- app/bet/[betId]/page.tsx
+++ app/bet/[betId]/page.tsx
@@ -1,5 +1,27 @@
 "use client";
 
+import { createBrowserClient } from "@supabase/ssr";
+import { useState, useEffect } from "react";
+
 export default function ViewBet({ params }: { params: { betId: number } }) {
-  return <div>{params.betId}</div>;
+  const [bet, setBet] = useState<any | null>(null);
+
+  // TODO: deduplicate
+  const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
+
+  useEffect(() => {
+    async function fetchData() {
+      const { error, data } = await supabase.from("bet").select("*").eq("id", params.betId).single();
+      setBet(data);
+
+      console.log("error and data are: ", error, data.title);
+    }
+
+    fetchData();
+  }, []);
+
+  return <div>{bet?.title}</div>;
 }
