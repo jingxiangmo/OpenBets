5f4b9850f3e7eb0eb61cbc95ce7badba9d455aaa
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-26 16:47:16 -0700

    move navbar footer to layout component - so they're on every page

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,17 @@ export default function RootLayout({
       <html lang="en" className={GeistSans.className}>
         <body className="bg-background text-foreground">
           <main className="min-h-screen flex flex-col items-center">
-            {children}
+            <div className="flex-1 w-full flex flex-col gap-20 items-center">
+              <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+                  <p className="text-xl"> OpenBets </p>
+                </div>
+              </nav>
+              {children}
+              <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+                <p>Made with ❤️ by Beau, JX, and Fraser</p>
+              </footer>
+            </div>
           </main>
         </body>
       </html>
--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,7 @@ import PastBets from "@/components/PastBets";
 
 export default async function Index() {
   return (
-    <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          <p className="text-xl"> OpenBets </p>
-        </div>
-      </nav>
-
+    <>
       <BetForm />
 
       <PastBets />
@@ -21,10 +15,6 @@ export default async function Index() {
 
       </SignedIn>
 
-    
-      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-        <p>Made with ❤️ by Beau, JX, and Fraser</p>
-      </footer>
-    </div>
+    </>
   );
 }
