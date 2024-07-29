ca0900a91cda1f6d26bb3176dd6a76b7fef57567
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-29 15:54:54 -0700

    create bet and wager query

--- db/queries.ts
+++ db/queries.ts
@@ -22,3 +22,31 @@ export async function deleteClerkUser(clerkId: string) {
     .delete(schema.usersTable)
     .where(eq(schema.usersTable.clerkId, clerkId));
 }
+
+export async function createBetAndWager(
+  clerkId: string, // creator of the bet also makes the initial wager
+  { title, resolveCondition, resolveDeadline }: schema.InsertBet,
+  { wager, side, odds }: schema.InsertWager,
+) {
+  return await db.transaction(async (tx) => {
+    const [{ insertedBetId }] = await tx
+      .insert(schema.betsTable)
+      .values({
+        createdById: clerkId,
+        title,
+        resolveCondition,
+        resolveDeadline,
+      })
+      .returning({ insertedBetId: schema.betsTable.id });
+
+    await tx.insert(schema.wagersTable).values({
+      betId: insertedBetId,
+      userId: clerkId,
+      wager,
+      side,
+      odds,
+    });
+
+    return insertedBetId;
+  });
+}
