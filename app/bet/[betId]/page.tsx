388b904e1c1a7c0c631476a9da26b271df223b6a
Beau McCartney <mccartney.beausl@gmail.com>
2024-07-30 11:33:26 -0700

    betpage uses new backend

--- app/bet/[betId]/page.tsx
+++ app/bet/[betId]/page.tsx
@@ -1,189 +1,125 @@
 "use client";
 
-import { createBrowserClient } from "@supabase/ssr";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { getBet } from "@/actions";
 
-import { userNamesFromIds } from "@/actions";
+import { BetInfoType } from "@/db/queries";
+import { updateBetResolutionFromBetPage } from "@/actions";
 
-interface BetInfo {
-  username: string;
-  wager: number;
-}
-
-interface BetsInfo {
-  title: string;
-  resolveCond?: string;
-  resolveStatus: number; // 0 = unresolved, 1 = resolved affirmatively, 2 = resolved negatively
-  affirmativeBets: BetInfo[];
-  negativeBets: BetInfo[];
+type ClientBetInfo = BetInfoType & {
   affirmativePot: number;
   negativePot: number;
   pot: number;
-  createdAt: string;
-}
+};
 
 export default function ViewBet({ params }: { params: { betId: number } }) {
-  const [bets, setBets] = useState<BetsInfo | undefined>(undefined);
+  const [bets, setBets] = useState<ClientBetInfo | undefined>(undefined);
   const [resolveChoice, setResolveChoice] = useState<number | null>(null);
-
-  const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  );
 
   useEffect(() => {
     async function fetchData() {
-      const { error, data } = await supabase
-        .from("bet")
-        .select("*")
-        .eq("id", params.betId)
-        .single();
+      // TODO: actual strategy for non-existent bet
+      const bet = (await getBet(params.betId))!;
 
-      if (error) {
-        console.error("Error fetching bet data:", error);
-        return;
-      }
-
-      function sum(nums?: number[]) {
-        return nums?.reduce((acc: number, val: number) => acc + val, 0) || 0;
-      }
-
-      const affirmativePot = sum(data.affirmative_user_wagers);
-      const negativePot = sum(data.negative_user_wagers);
+      const affirmativePot = bet.wagers
+        .filter((wager) => wager.side)
+        .reduce((sum, wager) => sum + wager.amountUSD, 0);
+      const negativePot = bet.wagers
+        .filter((wager) => !wager.side)
+        .reduce((sum, wager) => sum + wager.amountUSD, 0);
       const pot = affirmativePot + negativePot;
 
-      const affirmativeUsernames = await userNamesFromIds(
-        data.affirmative_user_clerk_ids,
-      );
-      const negativeUsernames = await userNamesFromIds(
-        data.negative_user_clerk_ids,
-      );
-
-      let affirmativeBets: BetInfo[] =
-        data.affirmative_user_wagers?.map((wager: number, index: number) => ({
-          username: affirmativeUsernames[index],
-          wager: wager,
-        })) || [];
-
-      let negativeBets: BetInfo[] = data.negative_user_wagers?.map(
-        (wager: number, index: number) => ({
-          username: negativeUsernames[index],
-          wager: wager,
-        }),
-      ) || [];
-
-      const bets: BetsInfo = {
-        title: data.title,
-        resolveCond: data.resolve_cond,
-        resolveStatus: data.resolve_status,
-        affirmativeBets,
-        negativeBets,
+      const betinfo = {
+        ...bet,
         affirmativePot,
         negativePot,
         pot,
-        createdAt: data.created_at,
+
       };
 
-      setBets(bets);
+      setBets(betinfo);
     }
 
     fetchData();
-  }, [supabase, params.betId]);
+  }, [params.betId]);
 
   if (!bets) {
-    return <div className="flex justify-center items-center h-screen">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
-    </div>;
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-blue-500"></div>
+      </div>
+    );
   }
 
   const handleResolve = async () => {
     if (resolveChoice === null) return;
 
-    const resolveStatus = resolveChoice === 2 ? 1 : resolveChoice === 1 ? 2 : 0;
-
-    const { error } = await supabase
-      .from("bet")
-      .update({
-        resolve_status: resolveStatus,
-      })
-      .eq("id", params.betId);
-
-    if (error) {
-      console.error("Error resolving bet:", error);
-    } else {
-      setBets(prev => prev ? {...prev, resolveStatus: resolveStatus} : prev);
-    }
+    await updateBetResolutionFromBetPage(params.betId, resolveChoice);
   };
 
   const handleUnresolve = async () => {
-    const { error } = await supabase
-      .from("bet")
-      .update({
-        resolve_status: 0,
-      })
-      .eq("id", params.betId);
+    await updateBetResolutionFromBetPage(params.betId, 0);
 
-    if (error) {
-      console.error("Error unresolving bet:", error);
-    } else {
-      setBets(prev => prev ? {...prev, resolveStatus: 0} : prev);
-      setResolveChoice(null);
-    }
+    setBets((prev) => (prev ? { ...prev, resolveStatus: 0 } : prev));
+    setResolveChoice(null);
   };
 
   const getResolveStatusColor = () => {
-    if (bets.resolveStatus === 1) return "bg-green-600";
-    if (bets.resolveStatus === 2) return "bg-red-600";
+    if (bets.resolved === 1) return "bg-red-600";
+    if (bets.resolved === 2) return "bg-green-600";
     return "bg-blue-600";
   };
 
   return (
-    <div className="min-h-screen py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-4xl mx-auto">
-        <div className="bg-white shadow-xl rounded-lg overflow-hidden">
+    <div className="min-h-screen px-4 py-12 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="overflow-hidden rounded-lg bg-white shadow-xl">
           <div className={`p-6 ${getResolveStatusColor()}`}>
-            <h1 className="text-3xl font-bold text-white mb-2">{bets.title}</h1>
-            <p className="text-xl text-white">{bets.resolveCond}</p>
-            {bets.resolveStatus !== 0 && (
+            <h1 className="mb-2 text-3xl font-bold text-white">{bets.title}</h1>
+            <p className="text-xl text-white">{bets.resolveCondition}</p>
+            {bets.resolved !== 0 && (
               <p className="mt-4 text-2xl font-bold text-white">
-                Resolved: {bets.resolveStatus === 1 ? "Yes" : "No"}
+                Resolved: {bets.resolved === 2 ? "Yes" : "No"}
               </p>
             )}
-            <p className="mt-2 text-sm text-white">Created: {new Date(bets.createdAt).toLocaleString()}</p>
+            <p className="mt-2 text-sm text-white">
+              Created: {new Date(bets.createdAt).toLocaleString()}
+            </p>
           </div>
-          
+
           <div className="p-6">
-            <div className="mb-6 bg-gray-50 p-4 rounded-lg">
-              <h2 className="text-xl font-semibold mb-2">Bet Details</h2>
+            <div className="mb-6 rounded-lg bg-gray-50 p-4">
+              <h2 className="mb-2 text-xl font-semibold">Bet Details</h2>
               <p className="text-lg">
-                <span className="font-medium">Odds:</span> {bets.affirmativePot} : {bets.negativePot}
+                <span className="font-medium">Odds:</span> {bets.affirmativePot}{" "}
+                : {bets.negativePot}
               </p>
               <p className="text-lg">
                 <span className="font-medium">Pot:</span> ${bets.pot.toFixed(2)}
               </p>
-
             </div>
-            
+
             <button
-              className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 mb-6"
+              className="mb-6 w-full rounded-md bg-blue-500 px-4 py-2 text-white transition duration-300 hover:bg-blue-600"
               onClick={(e) => {
                 const url = window.location.href;
                 navigator.clipboard.writeText(url);
                 (e.target as HTMLButtonElement).textContent = "Copied!";
                 setTimeout(() => {
-                  (e.target as HTMLButtonElement).textContent = "Copy Link to Bet";
+                  (e.target as HTMLButtonElement).textContent =
+                    "Copy Link to Bet";
                 }, 2000);
               }}
             >
               Copy Link to Bet
             </button>
 
-            <div className="mb-8 bg-white p-6 rounded-lg shadow-md">
-              <h2 className="text-2xl font-semibold mb-4">Resolve Bet</h2>
-              <div className="flex justify-center space-x-4 mb-4">
+            <div className="mb-8 rounded-lg bg-white p-6 shadow-md">
+              <h2 className="mb-4 text-2xl font-semibold">Resolve Bet</h2>
+              <div className="mb-4 flex justify-center space-x-4">
                 <button
-                  className={`px-6 py-3 rounded-full text-lg font-semibold transition-colors duration-300 ${
+                  className={`rounded-full px-6 py-3 text-lg font-semibold transition-colors duration-300 ${
                     resolveChoice === 2
                       ? "bg-green-500 text-white"
                       : "bg-gray-200 text-gray-700 hover:bg-gray-300"
@@ -193,7 +129,7 @@ export default function ViewBet({ params }: { params: { betId: number } }) {
                   Yes
                 </button>
                 <button
-                  className={`px-6 py-3 rounded-full text-lg font-semibold transition-colors duration-300 ${
+                  className={`rounded-full px-6 py-3 text-lg font-semibold transition-colors duration-300 ${
                     resolveChoice === 1
                       ? "bg-red-500 text-white"
                       : "bg-gray-200 text-gray-700 hover:bg-gray-300"
@@ -205,14 +141,14 @@ export default function ViewBet({ params }: { params: { betId: number } }) {
               </div>
               <button
                 onClick={handleResolve}
-                className="w-full px-6 py-3 bg-blue-500 text-white text-lg font-semibold rounded-md transition-colors duration-300 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+                className="w-full rounded-md bg-blue-500 px-6 py-3 text-lg font-semibold text-white transition-colors duration-300 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 disabled:cursor-not-allowed disabled:opacity-50"
                 disabled={resolveChoice === null}
               >
                 Confirm Resolution
               </button>
-              {bets.resolveStatus !== 0 && (
-                <p 
-                  className="mt-2 text-center text-sm text-blue-500 cursor-pointer hover:underline"
+              {bets.resolved !== 0 && (
+                <p
+                  className="mt-2 cursor-pointer text-center text-sm text-blue-500 hover:underline"
                   onClick={handleUnresolve}
                 >
                   Mark as unresolved
@@ -220,34 +156,49 @@ export default function ViewBet({ params }: { params: { betId: number } }) {
               )}
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-              <div className="bg-green-50 p-6 rounded-lg shadow-md">
-                <h2 className="text-2xl font-semibold mb-4 text-green-800">Affirmative Bets</h2>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+              <div className="rounded-lg bg-green-50 p-6 shadow-md">
+                <h2 className="mb-4 text-2xl font-semibold text-green-800">
+                  Affirmative Bets
+                </h2>
                 <ul className="space-y-2">
-                  {bets.affirmativeBets.map((bet, index) => (
-                    <li key={index} className="bg-white p-3 rounded shadow">
-                      <span className="font-semibold text-green-700">{bet.username}</span>
-                      <span className="float-right">${bet.wager.toFixed(2)}</span>
-                    </li>
-                  ))}
+                  {bets.wagers
+                    .filter((wager) => wager.side)
+                    .map((wager, index) => (
+                      <li key={index} className="rounded bg-white p-3 shadow">
+                        <span className="font-semibold text-green-700">
+                          {wager.user!.name}
+                        </span>
+                        <span className="float-right">${wager.amountUSD}</span>
+                      </li>
+                    ))}
                 </ul>
               </div>
-              <div className="bg-red-50 p-6 rounded-lg shadow-md">
-                <h2 className="text-2xl font-semibold mb-4 text-red-800">Negative Bets</h2>
+              <div className="rounded-lg bg-red-50 p-6 shadow-md">
+                <h2 className="mb-4 text-2xl font-semibold text-red-800">
+                  Negative Bets
+                </h2>
                 <ul className="space-y-2">
-                  {bets.negativeBets.map((bet, index) => (
-                    <li key={index} className="bg-white p-3 rounded shadow">
-                      <span className="font-semibold text-red-700">{bet.username}</span>
-                      <span className="float-right">${bet.wager.toFixed(2)}</span>
-                    </li>
-                  ))}
+                  {bets.wagers
+                    .filter((wager) => !wager.side)
+                    .map((wager, index) => (
+                      <li key={index} className="rounded bg-white p-3 shadow">
+                        <span className="font-semibold text-red-700">
+                          {wager.user!.name}
+                        </span>
+                        <span className="float-right">${wager.amountUSD}</span>
+                      </li>
+                    ))}
                 </ul>
               </div>
             </div>
           </div>
 
-          <div className="bg-gray-50 p-6 text-center border-t">
-            <Link href="/" className="text-blue-600 hover:underline font-medium">
+          <div className="border-t bg-gray-50 p-6 text-center">
+            <Link
+              href="/"
+              className="font-medium text-blue-600 hover:underline"
+            >
               Back to Home
             </Link>
           </div>
