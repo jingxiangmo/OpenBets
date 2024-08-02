8e0e6588e0cb040d049bf3706474e72f6914069c
Beau McCartney <mccartney.beausl@gmail.com>
2024-08-02 14:30:42 -0700

    prettier on layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
-import { ClerkProvider, SignedIn, SignedOut, SignOutButton, SignUpButton } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/react";
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignUpButton,
+} from "@clerk/nextjs";
 import Link from "next/link";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -26,22 +32,20 @@ export default function RootLayout({
           <main className="flex min-h-screen flex-col items-center">
             <div className="flex w-full flex-1 flex-col items-center gap-20">
               <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-
-
                 <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
-                  <Link href="/" className="text-xl">WeShallSee.xyz</Link>
+                  <Link href="/" className="text-xl">
+                    WeShallSee.xyz
+                  </Link>
                   <div>
                     <SignedIn>
                       <SignOutButton />
                     </SignedIn>
 
-              
                     <SignedOut>
                       <SignUpButton />
                     </SignedOut>
                   </div>
                 </div>
-
               </nav>
               {children}
               <footer className="flex w-full justify-center border-t border-t-foreground/10 p-8 text-center text-xs">
@@ -55,3 +59,4 @@ export default function RootLayout({
     </ClerkProvider>
   );
 }
+
