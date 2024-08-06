7b9d6dc46a8caa9b4adb81d0daa5643c7bb35bfc
Beau McCartney <mccartney.beausl@gmail.com>
2024-08-05 23:03:53 -0700

    in yes no radio control thecheckbox with a passed in hook

--- components/YesNoRadio.tsx
+++ components/YesNoRadio.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import { twMerge } from "tailwind-merge";
 
-type Props = React.HTMLAttributes<HTMLDivElement>;
+export type Choice = "yes" | "no" | null;
+
+type Props = React.HTMLAttributes<HTMLDivElement> & {
+  choice: Choice;
+  setChoice: (choice: Choice) => void;
+};
 
 // TODO: unique ids for the radio buttons
 
 export default function YesNoRadio({
+  choice,
+  setChoice,
   className,
   ...props
 }: Props) {
@@ -12,29 +21,33 @@ export default function YesNoRadio({
     <div className={twMerge("mb-4 flex text-center", className)} {...props}>
       <input
         id="yes"
-        className="peer/yes size-0"
+        className="size-0"
         type="radio"
         name="yesnoradio"
+        checked={choice === "yes"}
+        onChange={() => setChoice("yes")}
         required
       />
       <input
         id="no"
-        className="peer/no size-0"
+        className="size-0"
         type="radio"
         name="yesnoradio"
+        checked={choice === "no"}
+        onChange={() => setChoice("no")}
         required
       />
       <label
-        className="m-2 flex-grow rounded-md border-none bg-green-50 p-0 text-[#1e3050] peer-checked/yes:bg-green-500"
+        className="m-2 flex-grow rounded-md border-none bg-gray-700 p-0 text-[#1e3050]"
         htmlFor="yes"
       >
-        <span className="">✅ Yes</span>
+        <span className={`duration-40 block -translate-y-1.5 transform rounded-md border-none p-2.5 shadow transition-transform ease-linear hover:-translate-y-2 active:translate-y-0 ${choice === "yes" ? "bg-green-500" : "bg-green-50"}`}>✅ Yes</span>
       </label>
       <label
-        className="m-2 flex-grow rounded-md border-none bg-red-50 p-0 text-[#1e3050] peer-checked/no:bg-red-500"
+        className="m-2 flex-grow rounded-md border-none bg-gray-700 p-0 text-[#1e3050]"
         htmlFor="no"
       >
-        <span className="">❌ No</span>
+        <span className={`duration-40 block -translate-y-1.5 transform rounded-md border-none p-2.5 shadow transition-transform ease-linear hover:-translate-y-2 active:translate-y-0 ${choice === "no" ? "bg-red-500" : "bg-red-50"}`}>❌ No</span>
       </label>
     </div>
   );
