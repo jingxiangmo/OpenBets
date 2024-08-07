4dbfa4e5bad22cd22dfd171a716e723af8db8c9d
Jingxiang Mo <jingxiangmo@gmail.com>
2024-08-07 00:34:28 -0700

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,18 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "WeShallSee.xyz",
-  description: "The fastest way to settle a disagreement.",
+  title: "WeShallSee.xyz - The Fastest Way to Settle Disagreements",
+  description: "Create and manage friendly bets with your friends. Predict, and settle disagreements quickly and easily.",
+  keywords: "betting, friendly bets, disagreements, social betting, wagers",
+  openGraph: {
+    title: "WeShallSee.xyz - Friendly Betting Platform",
+    description: "Create and manage friendly bets with your friends. Settle disagreements quickly and easily.",
+  },
+  twitter: {
+    card: "summary",
+    title: "WeShallSee.xyz -  The Fastest Way to Settle Disagreements",
+    description: "Create and manage friendly bets with your friends. Predict, and settle disagreements quickly and easily.",
+  },
 };
 
 export default async function RootLayout({
@@ -25,9 +35,9 @@ export default async function RootLayout({
       <body className="bg-background text-foreground">
         <main className="flex min-h-screen flex-col items-center">
           <div className="flex w-full flex-1 flex-col items-center gap-20">
-            <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+            <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10" aria-label="Main Navigation">
               <div className="flex w-full max-w-4xl items-center justify-between p-3 text-sm">
-                <Link href="/" className="text-xl">
+                <Link href="/" className="text-xl" aria-label="WeShallSee.xyz Home">
                   WeShallSee.xyz
                 </Link>
                 <div className="flex items-center space-x-4">
