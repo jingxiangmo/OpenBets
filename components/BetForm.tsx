f092b6dfad89fecb92cff6e72d4df4a8cc14b2a3
Beau McCartney <mccartney.beausl@gmail.com>
2025-06-05 22:58:46 -0600

    Remove personal reference from bet page

--- components/BetForm.tsx
+++ components/BetForm.tsx
@@ -192,7 +192,7 @@ function BetFormInside() {
             value={topic}
             onChange={(e) => setTopic(e.target.value)}
             className="mt-1 block w-full rounded-md border border-gray-300 p-2 text-black placeholder-gray-400 focus:border-blue-500 focus:ring focus:ring-blue-200 focus:ring-opacity-50"
-            placeholder="Will Fraser and Guilia date by the end of 2024?"
+            placeholder="Will my favorite sports team make the playoffs?"
             rows={2}
             maxLength={4096}
             required
